ee4aef33ce0673f81920103e73a1b722c8902e5e
Dan Fessler <fezzinate@gmail.com>
2018-03-29 04:20:19 -0700

    start of day 21

--- 2017/21/solution.js
+++ 2017/21/solution.js
@@ -1,5 +1,20 @@
 input = document.body.textContent.trim();
 
+function ParseInput(input) {
+  return input.split(/\n/).map(
+    rule => {
+      var thisrule = rule.split(" => ").map(
+        row => row.split("/").map(
+          column => column.split('').map(
+            pixel => pixel == "#"? 1 : 0
+          )
+        )
+      )
+      return {input: thisrule[0], output: thisrule[1]}
+    }
+  )
+}
+
 // Part 1
 function Part1 (input) {
 
