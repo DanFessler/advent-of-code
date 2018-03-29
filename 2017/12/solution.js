237026eb8f964c9a5fe305021a498e798315aae0
Dan Fessler <fezzinate@gmail.com>
2018-03-29 01:56:15 -0700

    remaining puzzle stubs

---
+++ 2017/12/solution.js
@@ -0,0 +1,14 @@
+input = document.body.textContent.trim();
+
+// Part 1
+function Part1 (input) {
+
+}
+
+// Part 2
+function Part2 (input) {
+
+}
+
+console.log("DAY 12-1: "+Part1(input));
+console.log("DAY 12-2: "+Part2(input));
---
+++ 2017/16/solution.js
@@ -0,0 +1,14 @@
+input = document.body.textContent.trim();
+
+// Part 1
+function Part1 (input) {
+
+}
+
+// Part 2
+function Part2 (input) {
+
+}
+
+console.log("DAY 16-1: "+Part1(input));
+console.log("DAY 16-2: "+Part2(input));
