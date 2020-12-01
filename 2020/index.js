e943aa4749503d6739a15173b15246f6f91e85e2
DanFessler <fezzinate@gmail.com>
2020-12-01 15:48:50 -0800

    .

--- 2020/index.js
+++ 2020/index.js
@@ -3,23 +3,14 @@ var puzzles = ["./01", "./02"].map(file => require(file));
 let args = process.argv.slice(2);
 let day = args[0] ? parseInt(args[0], 10) : undefined;
 
-// if day was specified, only print that day's solution
-if (day) {
+if (day) solve(day);
+else puzzles.forEach((puzzle, i) => solve(i + 1));
+
+function solve(day) {
   console.log(
     `day_${day}_1:`,
     puzzles[day - 1][0](),
     `\nday_${day}_2:`,
     puzzles[day - 1][1]()
-  );
-}
-// Otherwise print all puzzle solutions
-else {
-  puzzles.forEach((puzzle, i) =>
-    console.log(
-      `day_${i + 1}_1:`,
-      puzzle[0](),
-      `\nday_${i + 1}_2:`,
-      puzzle[1]()
-    )
   );
 }
