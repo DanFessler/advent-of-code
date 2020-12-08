4e4829af8780244ce02043a08861816ccd995fd1
Dan Fessler <fezzinate@gmail.com>
2020-12-08 00:59:26 -0800

    day 8

---
+++ 2020/08/index.js
@@ -0,0 +1,54 @@
+// parse the input
+function Parse(input) {
+  return input.split("\r\n").map(cmd => cmd.split(" "));
+}
+
+// what's val of acc before repeating an instruction
+function Part1(input) {
+  let acc = 0;
+  let visited = {};
+
+  for (let i = 0; i < input.length; i++) {
+    if (visited[i] !== undefined) return acc;
+    visited[i] = true;
+
+    let [cmd, val] = input[i];
+    val = parseInt(val, 10);
+
+    switch (cmd) {
+      case "acc":
+        acc += val;
+        break;
+      case "jmp":
+        i += val - 1;
+        break;
+      case "nop":
+      default:
+        break;
+    }
+  }
+  return [acc, true];
+}
+
+// What's val of acc after patching in fixed instruction
+function Part2(input) {
+  for (let i = 0; i < input.length; i++) {
+    let inputCopy = [...input];
+    inputCopy[i] = [...inputCopy[i]];
+
+    if (inputCopy[i][0] === "nop") inputCopy[i][0] = "jmp";
+    else if (inputCopy[i][0] === "jmp") inputCopy[i][0] = "nop";
+
+    let result = Part1(inputCopy);
+    if (result[1]) return result[0];
+  }
+}
+
+// if we're running in the browser, parse the input from the document
+// otherwise export the functions
+if (typeof window !== "undefined") {
+  let input = Parse(document.body.textContent.trim());
+  console.log(Part1(input), Part2(input));
+} else {
+  module.exports = { Parse, Part1, Part2 };
+}
--- 2020/index.js
+++ 2020/index.js
@@ -5,21 +5,28 @@ let args = process.argv.slice(2);
 let day = args[0] ? parseInt(args[0], 10) : undefined;
 let inputPath = args[1] || "input.txt";
 
-var puzzles = ["./01", "./02", "./03", "./04", "./05", "./06", "./07"].map(
-  (file, i) => {
-    file = `${__dirname}/${file}`;
-    return {
-      input: fs
-        .readFileSync(
-          // if we're specifying a specific input file, use that instead for that day
-          path.resolve(file, day === i + 1 ? inputPath : "input.txt"),
-          "utf8"
-        )
-        .trim(),
-      ...require(file)
-    };
-  }
-);
+var puzzles = [
+  "./01",
+  "./02",
+  "./03",
+  "./04",
+  "./05",
+  "./06",
+  "./07",
+  "./08"
+].map((file, i) => {
+  file = `${__dirname}/${file}`;
+  return {
+    input: fs
+      .readFileSync(
+        // if we're specifying a specific input file, use that instead for that day
+        path.resolve(file, day === i + 1 ? inputPath : "input.txt"),
+        "utf8"
+      )
+      .trim(),
+    ...require(file)
+  };
+});
 
 if (day) solve(day);
 else puzzles.forEach((puzzle, i) => solve(i + 1));
