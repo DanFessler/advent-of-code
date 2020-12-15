eff0be3890906118501bd4df4f5fa98d0af0ee5e
Dan Fessler <fezzinate@gmail.com>
2020-12-15 00:33:12 -0800

    part 2

--- 2020/10/index.js
+++ 2020/10/index.js
@@ -3,14 +3,9 @@ function Parse(input) {
   return input.split(/\n/).map(n => parseInt(n, 10));
 }
 
-//
+// count total 1-diffs and 3-diffs, multiply together
 function Part1(input) {
-  const sorted = input.sort((a, b) => a - b);
-  const builtin = sorted[sorted.length - 1] + 3;
-
-  const diffs = [...sorted, builtin].map((jolts, i) => {
-    return i ? jolts - sorted[i - 1] : jolts;
-  });
+  const diffs = getDiffs(input);
 
   const diffCounts = diffs.reduce(
     (acc, val) => {
@@ -26,15 +21,47 @@ function Part1(input) {
     [0, 0]
   );
 
-  return sorted.map((elem, i) => `${elem}:${diffs[i]}`);
-  return [diffCounts, diffCounts[0] * diffCounts[1]];
   return diffCounts[0] * diffCounts[1];
+}
+
+function getDiffs(input) {
+  const sorted = input.sort((a, b) => a - b);
+  const builtin = sorted[sorted.length - 1] + 3;
+
+  return [...sorted, builtin].map((jolts, i) => {
+    return i ? jolts - sorted[i - 1] : jolts;
+  });
 }
 
 // can only get rid of indexes who's diffs are 1 and not before a 3
 // count all combinations of getting rid of those indexes
 // but can't get rid of more than 2 consecutive indexes
-function Part2(input) {}
+function Part2(input) {
+  let diffs = getDiffs(input);
+  let results = [];
+  let lastbit = null;
+
+  // build an array of consecutive 1-diff lengths
+  for (let i = 0; i < diffs.length; i++) {
+    let jolt = diffs[i];
+    if (jolt === 1) {
+      if (lastbit === 1) {
+        results[results.length - 1]++;
+      } else {
+        results.push(1);
+      }
+    }
+    lastbit = jolt;
+  }
+
+  // I worked out the permutations by hand for a string of consecutive 1-diffs up from 0 to 4
+  // which seems like the longest consecutive string present in my data
+  const permutationTable = [1, 2, 4, 7, 14];
+  results = results.map(val => permutationTable[val - 1]);
+
+  // multiply the permutations together
+  return results.reduce((acc, val) => acc * val);
+}
 
 // if we're running in the browser, parse the input from the document
 // otherwise export the functions
@@ -44,21 +71,3 @@ if (typeof window !== "undefined") {
 } else {
   module.exports = { Parse, Part1, Part2 };
 }
-
-// 3, 2
-// 2, 1
-// 4 * 2 = 6
-
-// consecutives:  4,4,3,2,4,1,4
-// minus one:     3,3,2,1,3,  3
-// binary +1:     8,8,4,2,8,  8
-
-let input = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4];
-// let input = [28,33,18,42,31,14,46,20,48,47,24,23,49,45,19,38,39,11,1,32,25,35,8,17,7,9,4,2,34,10,3]; //prettier-ignore
-
-// 4, 4, 3, 2, 4, 1, 4
-let consecutives = [1, 3, 2, 1]
-  .map(elem => Math.floor(elem / 2))
-  .reduce((acc, val) => acc * (val ? Math.pow(2, val) : 1));
-
-console.log(consecutives);
