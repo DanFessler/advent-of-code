5494dfef436497f2ca4171b405c8a3c4ce57937c
Dan Fessler <fezzinate@gmail.com>
2021-12-10 02:28:03 -0800

    Update index.js

--- 2021/10/index.js
+++ 2021/10/index.js
@@ -3,66 +3,56 @@ function Parse(input) {
   return input.split(/\r?\n/).map((row) => row.split(""));
 }
 
+const openings = "([{<";
+const closings = ")]}>";
+
 // Part 1
 function Part1(input) {
   let { errors } = solve(input);
 
   return errors.reduce((acc, tok) => {
-    return (
-      acc +
-      {
-        ")": 3,
-        "]": 57,
-        "}": 1197,
-        ">": 25137,
-      }[tok]
-    );
+    return acc + [3, 57, 1197, 25137][closings.indexOf(tok)];
   }, 0);
 }
 
 // Part 2
 function Part2(input) {
-  let { closings } = solve(input);
+  let { endings } = solve(input);
 
-  let scores = closings.map((closing) => {
-    return closing.reduce((acc, tok) => {
-      return acc * 5 + [")", "]", "}", ">"].indexOf(tok) + 1;
-    }, 0);
-  });
+  let scores = endings.map((ending) =>
+    ending.reduce((acc, tok) => {
+      return acc * 5 + closings.indexOf(tok) + 1;
+    }, 0)
+  );
 
   return scores.sort((a, b) => a - b)[Math.floor(scores.length / 2)];
 }
 
 function solve(input) {
   let errors = [];
-  let closings = [];
-
-  const map = {
-    "(": ")",
-    "[": "]",
-    "{": "}",
-    "<": ">",
-  };
+  let endings = [];
 
   for (line of input) {
     let stack = [];
 
     for (i in line) {
       const token = line[i];
-      if (["(", "[", "{", "<"].includes(token)) stack.push(map[token]);
-      if ([")", "]", "}", ">"].includes(token)) {
-        if (token !== stack.at(-1)) {
+      if (openings.includes(token)) {
+        stack.push(closings[openings.indexOf(token)]);
+      } else {
+        if (token === stack.at(-1)) stack.pop();
+        else {
           errors.push(token);
           break;
-        } else stack.pop();
+        }
       }
       if (parseInt(i) === line.length - 1) {
-        closings.push(stack.reverse());
+        endings.push(stack.reverse());
       }
     }
   }
 
-  return { errors, closings };
+  return { errors, endings };
 }
 
 // if we're running in the browser, parse the input from the document
